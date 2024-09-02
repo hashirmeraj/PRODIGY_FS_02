8eaaf1f83f8655173c853872b12de016b90bf758
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-02 11:58:30 +0500

    updated

--- index.php
+++ index.php
@@ -5,13 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./src/output.css" rel="stylesheet">
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
-    <header class=" bg-black w-full">
+    <header class=" bg-gray-500 w-full">
         <nav class="container mx-auto flex justify-between p-2">
             <div class="left"> <img src="./src/images/Nightmare Ninja.png" alt="" srcset=""></div>
-            <div class="right"></div>
+            <div class="right">
+                <div id="drop down">
+                    <span class="material-symbols-outlined text-red-200 ">
+                        keyboard_arrow_down
+                    </span>
+                    <ul class=" absolute hidden">
+                        <li>List 1</li>
+                        <li>List 1</li>
+                        <li>List 1</li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </header>
 
