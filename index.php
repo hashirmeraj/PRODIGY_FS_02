5ec3723749ecea9c6d48df5cb28a04601ff64b21
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-02 14:54:50 +0500

    updated

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="./src/output.css" rel="stylesheet">
     <script src="./src/script/script.js"></script>
     <!-- Icons -->
+    <script src="https://kit.fontawesome.com/b01bd35356.js" crossorigin="anonymous"></script>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
@@ -14,18 +15,51 @@
 <body>
     <div class="main flex">
         <!-- Sidebar -->
-        <?php
-        include './particles/sidebar.php'; ?>
+        <div class="relative h-screen w-1/5 bg-black ">
+            <div class="material flex flex-col items-center">
+                <div>
+                    <img src="./src/images/Nightmare Ninja.png" alt="" srcset="">
 
 
+                </div>
+
+            </div>
+        </div>
         <!-- Main Content Area -->
         <div class="flex flex-col w-4/5">
             <!-- Header -->
-            <?php
-            include './particles/header.php';
-            ?>
+            <header class="bg-gray-500 h-20 flex">
+                <nav class="container h-20 mx-auto flex justify-between items-center relative">
+                    <div class="left">
+                        <img src="./src/images/Nightmare Ninja.png" alt="Logo" class="h-10">
+                    </div>
+                    <div class="right flex items-center">
+                        <div class="image w-[50px] h-[50px] rounded-full">
+                            <img class="rounded-full"
+                                src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg"
+                                alt="">
+                        </div>
+                        <div id="dropDown" class="relative">
+                            <i id="dropdownButton" onclick="toggleDropdown()" class="fa-solid fa-sort-down select-none mt-3 w-[50px] h-[50px] rounded-full  text-red-200 cursor-pointer"></i>
 
-
+                            <ul id="dropDownMenu" class="hidden absolute right-0 mt-2 bg-white border rounded shadow-lg">
+                                <li class="px-4 py-2 hover:bg-gray-100 flex">
+                                    <span class="material-symbols-outlined">
+                                        manage_accounts
+                                    </span>
+                                    Profile
+                                </li>
+                                <li class="px-4 py-2 hover:bg-gray-100 flex">
+                                    <span class="material-symbols-outlined">
+                                        lock
+                                    </span>
+                                    Logout
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
             <!-- New Div for Main Content -->
             <div class="content bg-slate-400 flex-grow ">
 
--- particles/sidebar.php
+++ particles/sidebar.php
@@ -1,6 +1,6 @@
 <?php
 echo '
-        <div class="relative h-screen w-1/5 bg-black">
+        <div class="relative h-screen w-1/5 bg-black"> 
             <div class="absolute inset-y-0 left-0 w-16">04</div>
         </div>
     ';
