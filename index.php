95b950bba82a2d907abcdc9773db2e63be26856f
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-02 14:05:34 +0500

    updated

--- index.php
+++ index.php
@@ -5,29 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./src/output.css" rel="stylesheet">
+    <script src="./src/script/script.js"></script>
     <!-- Icons -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
-    <header class=" bg-gray-500 w-full">
-        <nav class="container mx-auto flex justify-between p-2">
-            <div class="left"> <img src="./src/images/Nightmare Ninja.png" alt="" srcset=""></div>
-            <div class="right">
-                <div id="drop down">
-                    <span class="material-symbols-outlined text-red-200 ">
-                        keyboard_arrow_down
-                    </span>
-                    <ul class=" absolute hidden">
-                        <li>List 1</li>
-                        <li>List 1</li>
-                        <li>List 1</li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <div class="main flex">
+        <!-- Sidebar -->
+        <?php
+        include './particles/sidebar.php'; ?>
 
+
+        <!-- Main Content Area -->
+        <div class="flex flex-col w-4/5">
+            <!-- Header -->
+            <?php
+            include './particles/header.php';
+            ?>
+
+
+            <!-- New Div for Main Content -->
+            <div class="content bg-slate-400 flex-grow ">
+
+                <div class=" bg-slate-700 h-12 w-max ab">akjaihsihsi</div>
+
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
---
+++ src/script/script.js
@@ -0,0 +1,14 @@
+
+function toggleDropdown() {
+    let dropDownMenu = document.getElementById('dropDownMenu');
+    dropDownMenu.classList.toggle("hidden");
+}
+
+document.addEventListener('click', function (event) {
+    let dropdownButton = document.getElementById('dropdownButton');
+    let dropdownMenu = document.getElementById('dropDownMenu');
+    // Check if the click is outside the dropdown button and menu
+    if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.classList.add('hidden');
+    }
+});
