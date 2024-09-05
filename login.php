0fa10fd2fca2ba152f09646235a1d220ea4c64ec
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-05 11:48:57 +0500

    updated

--- login.php
+++ login.php
@@ -29,23 +29,23 @@
         </div>
         <div class="p-4 text-xl  text-white"> Log in to access your account.</div>
         <div class="w-full max-w-xs">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form action="./particles/handleLogin.php" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                        Username
+                        Email
                     </label>
-                    <input required required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                    <input required required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="userEmail" type="email" placeholder="Email">
                 </div>
                 <div class="mb-6">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                         Password
                     </label>
 
-                    <input required required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                    <input required required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="userPassword" type="password" placeholder="******************">
                     <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
                 </div>
                 <div class="flex items-center justify-between">
-                    <button class="btn2" type="button">
+                    <button class="btn2" type="submit">
                         Sign In
                     </button>
 
---
+++ particles/handleLogin.php
@@ -0,0 +1,30 @@
+<?php
+include './config.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == 'POST') {
+
+    $useremail = $_POST['userEmail'];
+    $password = $_POST['userPassword'];
+
+    $sql = "SELECT * FROM `users1` WHERE `user_email` = '$useremail'";
+
+    $result = mysqli_query($conn, $sql);
+    $emailExist = mysqli_num_rows($result);
+
+    // checking email exit or not
+    if ($emailExist > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $loginPassword = $row['user_password'];
+        if (password_verify($password, $loginPassword)) {
+            session_start();
+
+            $_SESSION['loggedin'] = true;
+            $_SESSION['adminId'] = $row['user_id'];
+            $_SESSION['adminName'] = $row['user_name'];
+            header("Location:../index.php");
+        }
+    }
+}
+// header("Location:../login.php?success=false&&error=" . urlencode($error));
+// exit();
