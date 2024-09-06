765a6adabc825b6e291c101a6f9867e765fb1a2e
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-06 11:11:16 +0500

    Update addEmp.php

--- addEmp.php
+++ addEmp.php
@@ -115,7 +115,7 @@ if (isset($_GET['success']) && $_GET['success'] == "false") {
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
             </svg>
             <div class="ms-3 text-sm font-medium">
-                Employee details added successfully!
+                Employee details updated successfully.
             </div>
             <button id="close" type="button" class="success-close" data-dismiss-target="#alert-border-1" aria-label="Close">
                 <span class="sr-only">Dismiss</span>
