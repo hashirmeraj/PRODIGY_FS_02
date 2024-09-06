a698339cc6ace0f122f4d4616b7a39c7057f8f8c
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-06 11:20:22 +0500

    Update addEmp.php

--- addEmp.php
+++ addEmp.php
@@ -91,14 +91,14 @@ if (isset($_GET['success']) && $_GET['success'] == "false") {
             </button>
         </div>
             ';
-                } elseif ($$updateError) {
+                } elseif ($updateError) {
                     echo '
                     <div id="alert-border-1" class="error alert-border-1" role="alert">
             <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
             </svg>
             <div class="ms-3 text-sm font-medium">
-                Employee already exists. Please check the details and try again.
+                An error occurred while updating employee details.
             </div>
             <button id="close" type="button" class="error-close" data-dismiss-target="#alert-border-1" aria-label="Close">
                 <span class="sr-only">Dismiss</span>
